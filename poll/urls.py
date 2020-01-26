419e60c6a71f741e38d87bdbd6654a19f718200e
zychin2000 <zychin2000@gmail.com>
2020-01-26 13:31:51 -0800

    just finished the post views

--- poll/urls.py
+++ poll/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('vote', views.vote, name='vote')
 ]
 
--- poll/views.py
+++ poll/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Coral, User
+from .models import Coral, User, Answer
 from .utils import get_random_coral
-
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 # Create your views here.
 
@@ -16,3 +17,28 @@ def index(request):
 
     context = {'currentcoral': currentcoral}
     return HttpResponse(template.render(context, request))
+
+
+def vote(request):
+
+    try:
+        current_coral_id = request.POST['current_coral_id']
+        user_selected_health = request.POST['submitbutton']
+    except(KeyError, Coral.DoesNotExist):
+        return render(request, 'poll', {
+            'error_message': "There was an unexpected error saving the information.",
+        })
+    else:
+        coral = get_object_or_404(Coral, pk=current_coral_id)
+        if user_selected_health == 'Yay!':
+            new_answer = Answer(coral=coral, choice='HEALTHY')
+            new_answer.save()
+            return HttpResponseRedirect(reverse('index'))
+        elif user_selected_health == "Nay!":
+            new_answer = Answer(coral=coral, choice='UNHEALTHY')
+            new_answer.save()
+            return HttpResponseRedirect(reverse('index'))
+
+
+
+
